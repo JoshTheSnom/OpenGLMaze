8da12ad8397bd7b58d830134d5b99420a3462bfb
Kreggy <filip.makrlik@gmail.com>
2021-03-23 14:39:24 +0100

    crazy gradient

--- src/main/java/educanet/Game.java
+++ src/main/java/educanet/Game.java
@@ -9,11 +9,9 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.Random;
 
 public class Game {
-
-    private static Square square;
-    private static Square square2;
 
     public static String mazeInput = "101111111000111011101010000001\n" +
             "100000101101101100111011111111\n" +
@@ -49,6 +47,18 @@ public class Game {
     public static String mazeText = mazeInput.replaceAll("\n", "");
     public static float squareSize = 1.0f/((float)size/2);
     public static Square[][] mazeArray = new Square[size][size];
+    public static Square gradient;
+    public static boolean goDown;
+    public static float[] grd = {
+            /*0.0f, 0.0f, 0.5f,
+            0.0f, 0.5f, 0.0f,
+            0.5f, 0.0f, 0.0f,
+            0.5f, 0.5f, 0.0f,*/
+            0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+    };
     public static void init(long window) {
 
         System.out.println(squareSize);
@@ -60,24 +70,21 @@ public class Game {
                 1.0f, 1.0f, 1.0f,
         };
 
-        float[] black = {
-                0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f, 0.0f,
-        };
         float xCord = -1.0f;
         float yCord = 1.0f-squareSize;
 
         int codePos = 0;
+        gradient = gradient(grd);
         for(int y = 0; y<size; y++) {
-            for(int x = 0; x<size; x++) {
+            for (int x = 0; x < size; x++) {
                 Square square;
-                if(mazeText.charAt(codePos) == '1') square = createSquare(xCord, yCord, white);
-                else square = createSquare(xCord, yCord, black);
+                if (mazeText.charAt(codePos) == '1') {
+                    square = createSquare(xCord, yCord, white);
+                }
+                else square = createSquare(xCord, yCord, grd);
+                mazeArray[y][x] = square;
                 codePos++;
 
-                mazeArray[y][x] = square;
 
                 xCord += squareSize;
             }
@@ -85,7 +92,24 @@ public class Game {
             yCord -= squareSize;
         }
 
+
         Shaders.initShaders();
+    }
+
+    private static Square gradient(float[] color) {
+        int[] indices = {
+                0, 1, 3, // First triangle
+                1, 2, 3 // Second triangle
+        };
+
+        float[] vertices = {
+                1.0f, 1.0f, 0.0f, // 0 -> Top right
+                1.0f, -1.0f, 0.0f, // 1 -> Bottom right
+                -1.0f, -1.0f, 0.0f, // 2 -> Bottom left
+                -1.0f, 1.0f, 0.0f, // 3 -> Top left
+        };
+
+        return new Square(vertices, indices, color);
     }
 
     private static Square createSquare(float x, float y, float[] color) {
@@ -100,12 +124,24 @@ public class Game {
                 x, y, 0.0f, // 2 -> Bottom left
                 x, y + squareSize, 0.0f, // 3 -> Top left
         };
-        return new Square(vertices, indices, color);
+        float[] vertices2 = {
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+        };
+
+
+        if(color[3] != 0.0f) return new Square(vertices, indices, color);
+        else return new Square(vertices2, indices, color);
+
     }
 
     public static void render(long window) {
         GL33.glUseProgram(Shaders.shaderProgramId);
 
+        GL33.glBindVertexArray(gradient.vaoId);
+        GL33.glDrawElements(GL33.GL_TRIANGLES, gradient.indices.length, GL33.GL_UNSIGNED_INT, 0);
         // Draw using the glDrawElements function
         for(int y = 0; y<size; y++) {
             for(int x = 0; x<size; x++) {
@@ -114,9 +150,27 @@ public class Game {
             }
         }
 
+
     }
 
     public static void update(long window) {
+        float color = 0.01f;
+        Random rand = new Random();
+        int i = rand.nextInt(12);
+        //for(int i=0; i<grd.length; i+= (Math.random() * ((3 - 1) + 1)) + 1) {
+            if(grd[i] >= 1.0f) goDown = true;
+            if(grd[i] <= 0.0f) goDown = false;
+            //if(grd[i] == 0 | grd[i] == 3 | grd[i] == 6 | grd[i] == 9) color = 0.03f;
+            else color = 0.05f;
+            if(goDown) grd[i] -= color;
+            else grd[i] += color;
+        //}
+        /*System.out.println(grd[1]);
+        if(grd[1] >= 1.0f) goDown = true;
+        else if(grd[1] <= 0.0f) goDown = false;
+        if(goDown) grd[1] -= 0.01f;
+        else grd[1] += 0.01f;*/
+        gradient = gradient(grd);
 
     }
 
